15529940aa9effe39303bdb4e884fcef62aeb44f
KD <kuldeeparyadotcom@gmail.com>
2016-07-17 14:42:32 -0500

    Task API is in good shapre: 1. Get all tasks of specific user 2. Get details of a specific task 3. Put request to update a specific task 4. Post request to create a new task 5. Delete request to delete a specific task All there scenarios are working fine. Error handling is to be put in place though.

--- tasksmanager/routes/index.js
+++ tasksmanager/routes/index.js
@@ -38,8 +38,8 @@ db.once('open', function()
   connectionStatus = 'connected';
 
   tasksSchema = mongoose.Schema({
-    task: String,
     user: String,
+    task: String,
     priority: Number,
     iscompleted: Boolean
   });
@@ -51,39 +51,58 @@ console.log('Model Task is created');
 
 //display all tasks
 router.get('/tasks/:user', function(req, res, next) {
-  res.setHeader('Content-Type', 'text/html');
-  res.send('Display all tasks for the user: ' + req.params.user );
+  var received_user = req.params.user;
+  Task.find( { user: received_user },function(err, tasks) {
+    if (err) return console.error(err);
+    console.log(tasks);
+    //var user_tasks = JSON.stringify(tasks);
+
+    //res.setHeader('Content-Type', 'application/json');
+    console.log(tasks);
+    res.json(tasks);
+  })
+
 });
 
 
 //Provide details of a specific task
 router.get('/task/:id', function(req, res){
-  res.send('Display detailed view of given task: ' + req.params.id);
+  var received_id = req.params.id;
+  Task.find( { _id: received_id }, function(err, task) {
+    if (err) return console.error(err);
+    console.log(task);
+    //var specific_task = JSON.stringify(task);
+
+    //res.setHeader('Content-Type', 'application/json');
+    console.log(task);
+    res.json(task);
+  })
+  //res.send('Display detailed view of given task: ' + req.params.id);
 });
 
 
 //Add a new task
-router.post('/task/:user', function(req,res) {
-  res.send('Post a task for logged in user ' + req.params.user );
+router.post('/task', function(req,res) {
    if (connectionStatus === 'connected') {
       console.log('Trying to add a new task');
       console.log(req.body);
+      console.log(req.body.user);
       console.log(req.body.task);
       console.log(req.body.priority);
+      console.log(req.body.iscompleted);
      var task = new Task({
-        //task: 'Enjoy Priority',
-        user: req.params.user,
+        user: req.body.user,
         task: req.body.task,
-        //priority: 1,
         priority: req.body.priority,
-        iscompleted: false
+        iscompleted: req.body.iscompleted
       });
 
       //Save to Mongo DB only if all request parametes are present in JSON body
-      if ( req.body.task !== undefined && req.body.priority !== undefined) {
+      if ( req.body.task !== undefined && req.body.priority !== undefined && req.body.user !== undefined && req.body.iscompleted !== undefined ) {
         task.save(function(err, task) {
         if (err) return console.error(err);
         console.log(task);
+        res.json(task);
         });
       }else {
         console.log('Task and Priority must be provided to save data to Mongo!');
@@ -97,14 +116,67 @@ router.post('/task/:user', function(req,res) {
 
 
 //Update a given task
-router.put('/task/:id', function(req, res) {
-  res.send('Update the task for logged in user '  + req.params.id  );
+router.put('/task', function(req, res) {
+  var received_id = req.body.id;
+  console.log('received_id: ' + received_id);
+
+  Task.findById(received_id, function(err, task) {
+    if (err) return console.error(err);
+    
+    //Update task description if it is provided in body
+    if (req.body.task !== undefined) {
+      task.task = req.body.task;
+    } else {
+      console.log('task description is not to be updated');
+    }
+
+    //Update task priority if provided in body
+    if (req.body.priority !== undefined) {
+      task.priority = req.body.priority;
+    } else {
+      console.log('task priority is not to be updated');
+    }
+
+    //Update iscompleted task status only if provided in body
+    if (req.body.iscompleted !== undefined) {
+      task.iscompleted = req.body.iscompleted;
+    } else {
+      console.log('task completion status is not to be updated');
+    }
+
+    //Save and return updated document
+    task.save(function(err) {
+      if (err) return console.error(err);
+      res.json(task);
+    });
+
+  });
+
 });
 
 
 //Delete a given task
-router.delete('/task/:id', function(req, res) {
-  res.send('Delete the given task ' + req.params.id );
+router.delete('/task', function(req, res) {
+  var received_id = req.body.id;
+  console.log('received_id: ' + received_id);
+  
+   Task.findById(received_id, function(err, task) {
+    if (err) return console.error(err);
+
+    if (task !== null) {
+      task.remove(function(err, deleted_task) {
+        if (err) return console.error(err);
+        console.log('Document removed: ' + deleted_task);
+        res.send(deleted_task);
+      });
+    }else {
+      console.log('Invalid Task ID provided!');
+      res.send('Invalid Task ID provided!');
+    }
+    //remove requested task (document from mongoose db)
+
+   });
+  //res.send('Delete the given task ' + req.body.id );
 });
 
 module.exports = router;
